e4f5d68d77aa29ac6ba69772d67b8adafa52dff0
Anand-S23 <anandsroopsingh23@gmail.com>
2023-02-02 17:56:54 -0500

    Moving old engine from main to old

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -xe
+
+mkdir -p build
+cd build
+
+CC=gcc
+CFLAGS="-Wall -Wextra -Wno-address -std=c11 -ggdb"
+LIBS="-lGL -lSDL2 -lm -I../third_party"
+
+src=../src/drift_entrypoint.c
+name=drift
+
+$CC $CFLAGS -o $name $src ../third_party/glew.c $LIBS
+
+cd ..
