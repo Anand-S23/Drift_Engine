ddcd2ad8c158dcaacbaaac581947a068238e02aa
Anand-S23 <anandsroopsingh23@gmail.com>
2023-02-28 23:32:07 -0500

    Drift math library

--- test_game/app.c
+++ test_game/app.c
@@ -1,15 +1,23 @@
 #include <drift.h>
 #include <stdio.h>
+#include <assert.h>
 
 #include "app.h"
 
+static app_t *app;
+
 DRIFT_APP_ENTRY_POINT void init_app(drift_platform_t *platform)
 {
+    assert(sizeof(app_t) <= platform->permanent_storage_size);
+    app = platform->permanent_storage;
     platform->initialized = 1;
 }
 
 DRIFT_APP_ENTRY_POINT void update_app(drift_platform_t *platform)
 {
+    app->x += 5;
+    printf("%d\n", app->x);
+
     printf("%dms/f, %dfps, %dmc/f %lfs\n",
            platform->ms_per_frame,
            platform->fps,
---
+++ test_game/app.h
@@ -0,0 +1,9 @@
+#ifndef APP_H
+#define APP_H
+
+typedef struct app
+{
+    u8 x;
+} app_t;
+
+#endif
